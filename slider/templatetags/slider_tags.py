9dfdc6fd58a2e5a67b9409b8fbb30e890e38086f
Krozark <maxime.barbier1991@gmail.com>
2012-06-01 16:38:40 -0400

    add correct random

--- slider/templatetags/slider_tags.py
+++ slider/templatetags/slider_tags.py
@@ -1,12 +1,30 @@
 # -*- coding: utf-8 -*-
 from django import template
 from slider.models import SliderImage
+import random
 
 register = template.Library()
+
+def get_random_item(l,max=None):
+    res= []
+    size = len(l)
+    indexs = range(0,size)
+    if max = None:
+        max = size
+
+    for i in range(0: max):
+        index = random.choice(indexs)
+        indexs.pop(index)
+        res += l[index]
+    return res
 
 @register.assignment_tag
 def get_slider_images(limit=False, randomize=True):
     qs = SliderImage.objects.filter(is_visible=True)
-    if randomize is True:  qs = qs.order_by('?')
-    if limit is not False: qs = qs[0:limit]
+    if randomize is True and limit is True :
+        qs = get_random_item(qs,limit)
+    elif randomize is True:  
+        qs = get_random_item(qs)
+    if limit is not False: 
+        qs = qs[0:limit]
     return qs
