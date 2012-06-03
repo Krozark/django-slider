521b787b6374408f5907415e64dc4f60eb153366
krozark <maxime.barbier1991@gmail.com>
2012-06-02 23:34:04 -0400

    add admin.py

---
+++ slider/admin.py
@@ -0,0 +1,8 @@
+# -*- encoding: UTF-8 -*-
+
+from django.contrib import admin
+from slider.models import *
+
+class SliderAdmin(admin.ModelAdmin):
+	list_display= ('slider','is_visible')
+admin.site.register(Slider,SliderAdmin)
