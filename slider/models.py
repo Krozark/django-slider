af5dba70a159734e0b7719e12a60394e3a0d6046
Maxime Barbier <maxime.barbier1991@gmail.com>
2012-06-03 00:27:15 -0300

    add multi slider gestion

--- slider/models.py
+++ slider/models.py
@@ -4,6 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 class SliderImage(models.Model):
     image = models.ImageField(_("Image"),upload_to="uploads/slider")
     is_visible = models.BooleanField(_("Is Visible"), default=True)
+    slider = models.IntegerFiel(_('Slider number'),default=1)
 
     def __unicode__(self):
         return u"%d" % self.id
