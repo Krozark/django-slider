0318b2ed437c72a24b9365ba089515e0888532be
Maxime Barbier <maxime.barbier1991@gmail.com>
2012-06-05 11:38:27 -0300

    Update master

--- slider/templatetags/slider_tags.py
+++ slider/templatetags/slider_tags.py
@@ -1,30 +1,15 @@
 # -*- coding: utf-8 -*-
 from django import template
 from slider.models import SliderImage
-import random
-
 register = template.Library()
-
-def get_random_item(l,max=None):
-    res= []
-    size = len(l)
-    indexs = range(0,size)
-    if max == None:
-        max = size
-
-    for i in range(0, max):
-        index = random.choice(indexs)
-        indexs.pop(index)
-        res += l[index]
-    return res
 
 @register.assignment_tag
 def get_slider_images(limit=False, randomize=True, slider=1):
     qs = SliderImage.objects.filter(is_visible=True,slider=slider)
-    if randomize is True and limit is True :
-        qs = get_random_item(qs,limit)
-    elif randomize is True:  
-        qs = get_random_item(qs)
-    if limit is not False: 
+    
+    if randomize:
+        qs = qs.order_by('?')
+    if limit is not False:
         qs = qs[0:limit]
+    
     return qs
