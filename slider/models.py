e550ddeced649c1c19e2df48a5ca88512159262c
Dominic Montreuil <dominic.montreuil@gmail.com>
2012-06-11 14:40:38 -0300

    Update master

--- slider/models.py
+++ slider/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.db.models.signals import post_delete
+from slider.utils import file_cleanup
 
 class SliderImage(models.Model):
     image = models.ImageField(_("Image"),upload_to="uploads/slider")
@@ -12,3 +14,4 @@ class SliderImage(models.Model):
     class Meta:
         verbose_name = _("Slider image")
         verbose_name_plural = _("Slider Images")
+post_delete.connect(file_cleanup, sender=SliderImage, dispatch_uid="SliderImage.file_cleanup")
