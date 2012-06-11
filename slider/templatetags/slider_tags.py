6de5ab2892a9b41f781db57fa574ffe290fcfbab
Krozark <maxime.barbier1991@gmail.com>
2012-06-11 14:25:32 -0400

    add exemple template

--- slider/templatetags/slider_tags.py
+++ slider/templatetags/slider_tags.py
@@ -8,6 +8,8 @@ def get_slider_images(limit=False, randomize=True, slider=1):
 
     qs = SliderImage.objects.filter(is_visible=True,slider=slider)
     
+    print randomize
+
     if randomize:
         qs = qs.order_by('?')
 
