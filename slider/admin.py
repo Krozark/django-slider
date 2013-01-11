37116269d9d361d7fd89a74281014555d3fed52e
Dominic Montreuil <dominic.montreuil@gmail.com>
2013-01-11 14:10:37 -0500

    showing the thumbnail in the admin

--- slider/admin.py
+++ slider/admin.py
@@ -3,6 +3,10 @@
 from django.contrib import admin
 from slider.models import *
 
-class SliderAdmin(admin.ModelAdmin):
-	list_display= ('slider','is_visible')
+from webcore.utils.admin import AdminThumbnailMixin
+
+class SliderAdmin(admin.ModelAdmin, AdminThumbnailMixin):
+	list_display= ('thumbnail','is_visible')
+    thumbnail_options = {'size': (100,100), 'crop': True}
+
 admin.site.register(SliderImage,SliderAdmin)
